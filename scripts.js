1661c0196cb2a89938bdb8aead0821221912de3a
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-05-24 15:27:53 -0500

    Terminacion de detalles

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 const hamburguesa = document.querySelector('.hamburguesa');
-
+const burgerIco = document.getElementById('burgerIco');
 const menuMobile = document.querySelector('.ulMobile');
 
+let isOpen = false;
+
 hamburguesa.addEventListener('click', (e) => {
-    menuMobile.classList.toggle('inactive')
-})
+    menuMobile.classList.toggle('inactive');
+    
+    if (isOpen) {
+        burgerIco.src = 'icon-hamburger.svg';
+    } else {
+        burgerIco.src = 'resources/icon-close.svg';
+    }
+    
+    isOpen = !isOpen;
+});
 
